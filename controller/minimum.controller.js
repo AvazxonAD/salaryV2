7255b52b3a29ba278e178e74e864e74c17ae4d5e
AvazxonAD <asqarovavazxon77@gmail.com>
2024-05-17 21:52:58 +0500

    salary

--- controller/minimum.controller.js
+++ controller/minimum.controller.js
@@ -2,6 +2,7 @@ const asyncHandler = require('../middleware/async')
 const ErrorResponse = require('../utils/errorResponse')
 const Minimum = require('../models/minimum')
 const Master = require('../models/master.model')
+const Position = require('../models/position.model') 
 
 // get minimum 
 exports.getMinimum = asyncHandler(async (req, res, next) => {
@@ -18,5 +19,10 @@ exports.updateMinimum = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse("Sizga bu funksiya bajarish ruhsat etilmagan", 403))
     }
     const minimum = await Minimum.findByIdAndUpdate(req.params.id, {summa}, {new : true})
+    const positions = await Position.find()
+    for(let position of positions){
+        position.salary = position.percent * minimum.summa
+        await position.save()
+    }
     return res.status(200).json({success : true, data : minimum})
 })
